2534eca4a5fd1f00624c90a86be0ad4f291270e2
Dylan Brown <dylan.brown@users.noreply.github.com>
2019-02-26 02:15:35 -0600

    lesson

--- Lessons/lesson13/maxInterval_skeleton.cpp
+++ Lessons/lesson13/maxInterval_skeleton.cpp
@@ -12,13 +12,13 @@ void maxInterval1(int* data, int size) {
 	int sum = 0;
 
 	for(int i = 0; i<size; i++){
-		cout << "I: " << i;
+		//cout << "I: " << i;
 		for(int j = i; j<size; j++){
-			cout << " J: " << j;
+			//cout << " J: " << j;
 			for(int k=i; k<=j; k++){
-				cout << " K: " << k << endl;
+				//cout << " K: " << k << endl;
 				sum+=data[k];
-				cout << "Sum: " << sum << endl;
+				//cout << "Sum: " << sum << endl;
 				}
 				if(sum>maxSum){
 					maxSum = sum;
@@ -29,10 +29,6 @@ void maxInterval1(int* data, int size) {
 			}
 	}
 
-	/*	if(sum > maxSum){
-			maxSum = sum;
-			minIndex = data[k];
-			maxIndex = data[j];*/
 
 	cout << "The maximum sum is: " << maxSum << " which occurs between " << minIndex << " and " << maxIndex << endl;
 }
@@ -42,6 +38,13 @@ void maxInterval2(int* data, int size) {
 	int minIndex;
 	int maxIndex;
 	int maxSum;
+	int oldSum = 0;
+	int newSum = 0;
+	for(int i = 0; i<size; i++){
+		for(int j = i; j<size; j++){
+
+		}
+	}
 
 
 	cout << "The maximum sum is: " << maxSum << " which occurs between " << minIndex << " and " << maxIndex << endl;
@@ -67,15 +70,16 @@ int main ( int argc, char *argv[] ) {
 
 	}
 	for (int i=0; i < n; i++) {
-		cout << dataArray[i] << endl;
+		cout << dataArray[i] << " ";
 	}
+	cout << endl;
 	//*************************
 
 	clock_t t;
     t = clock();  //set up and start clock
 
-	maxInterval1(dataArray, n);  // call the function of interest
-
+	//maxInterval1(dataArray, n);  // call the function of interest
+	maxInterval2(dataArray, n);  // call the function of interest
 	t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC; // stop clock
 
